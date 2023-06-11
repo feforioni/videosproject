4d7f87bc0c40bb1430218c7667e99a1ff8169c48
Felipe Lima <fe.forioni@gmail.com>
2023-06-11 18:26:03 -0300

    Implement the MVC architecture.

---
+++ YoutubeProject/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  YoutubeProject
+//
+//  Created by Felipe Lima on 26/05/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ YoutubeProject/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  YoutubeProject
+//
+//  Created by Felipe Lima on 26/05/23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+       
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
