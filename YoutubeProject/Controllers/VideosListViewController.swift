9f92777f6a0429d68e838ddabff0903ea8a0460f
Felipe Lima <fe.forioni@gmail.com>
2023-06-15 18:45:41 -0300

    Create videos list template

--- YoutubeProject/Controllers/VideosListViewController.swift
+++ YoutubeProject/Controllers/VideosListViewController.swift
@@ -6,6 +6,7 @@ class VideosListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -14,8 +15,18 @@ class VideosListViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return 15
     }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "meuIdentificador")!
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 318
+    }
+    
     @IBAction func onLoginButtonTapped(_ sender: Any) {
         navigationController?.pushViewController(LoginViewController(), animated: true)
     }
---
+++ YoutubeProject/View/VideoTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  VideoTableViewCell.swift
+//  YoutubeProject
+//
+//  Created by Felipe Lima on 14/06/23.
+//
+
+import UIKit
+
+class VideoTableViewCell: UITableViewCell {
+
+
+    @IBOutlet weak var channelLogoImageView: UIImageView!
+    @IBOutlet weak var channelAndViewLabel: UILabel!
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    
+    @IBOutlet weak var tittleVideoLabel: UILabel!
+}
